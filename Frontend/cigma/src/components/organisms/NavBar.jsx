a2b02e2074c6b8ab96c7cdb8c3082f2491893223
yoonjinlee <yoonjinifer@gmail.com>
2023-04-21 00:23:12 +0900

    [S08P31A601-104] [FE]:[feat] 네비게이션 바 타이틀 컴포넌트 수정 #in-progress

--- Frontend/cigma/src/components/organisms/NavBar.jsx
+++ Frontend/cigma/src/components/organisms/NavBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../styles/organisms/NavBar.scss';
 import NavItem from '../atoms/NavItem';
+import NavLogo from "../atoms/NavLogo";
 function NavBar() {
     return (
         <nav>
+            <NavLogo/>
             <NavItem itemText={'Logo 들어갈 자리'}></NavItem>
             <NavItem itemText={'test'}></NavItem>
         </nav>
