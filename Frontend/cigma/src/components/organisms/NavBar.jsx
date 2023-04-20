2fd80c67ec9f0cfb66d6f9fe11f18cd56653d923
yoonjinlee <yoonjinifer@gmail.com>
2023-04-21 01:42:05 +0900

    [S08P31A601-104] [FE]:[chore] 주석작성  #in-progress

--- Frontend/cigma/src/components/organisms/NavBar.jsx
+++ Frontend/cigma/src/components/organisms/NavBar.jsx
@@ -3,6 +3,11 @@ import '../../styles/organisms/NavBar.scss';
 import NavItem from '../atoms/NavItem';
 import NavLogo from "../atoms/NavLogo";
 import NavMenu from "./NavMenu";
+
+/**
+ * 
+ * @returns StartPage에서 사용되는 네비게이션 바. 로고 + 중간메뉴 + 로그인버튼
+ */
 function NavBar() {
     return (
         <nav>
--- Frontend/cigma/src/components/organisms/NavMenu.jsx
+++ Frontend/cigma/src/components/organisms/NavMenu.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../../styles/organisms/NavMenu.scss';
 import NavItem from "../atoms/NavItem";
+/**
+ * 
+ * @returns 단일NavItem 컴포넌트를 한번에 묶어서 NavMenu 컴포넌트 생성
+ */
 function NavMenu() {
   return (
     <div className="menu">
