407ce4b62d3dfebfddcae78919d250ab24f8fabf
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-04-20 17:10:30 +0900

    [S08P31A601-102] [FE]:[feat] LoginPage 디자인 #done

---
+++ Frontend/cigma/src/components/pages/LoginPage.jsx
@@ -0,0 +1,28 @@
+// LoginForm.jsx
+import React from "react";
+import "../../styles/pages/LoginPage.scss";
+import InputAtom from "../atoms/InputAtom";
+import Button from "../atoms/Button";
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h1>돌아오신 것을 환영해요!{<br />}다시 한 번 Cigma를 느껴보세요.</h1>
+        <InputAtom id="email" type={"email"} label={"이메일"} />
+        <InputAtom id="password" type={"password"} label={"비밀번호"} />
+        <div className="auto-login">
+          <input type="checkbox" id="autoLogin" />
+          <label htmlFor="autoLogin">자동로그인</label>
+        </div>
+        <Button buttonName={"로그인"} />
+        <div className="go-signup">
+          <p>계정이 필요하신가요?</p>
+          <p style={{ color: "#5f6f90", fontWeight: "bold" }}>가입하기</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
