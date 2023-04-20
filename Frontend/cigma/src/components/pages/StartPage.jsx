55a5d14a4eff4b8e06f742fb0c551fe456cbdef8
2yoonjinny <2yoonjinny@naver.com>
2023-04-20 17:36:07 +0900

    [S08P31A601-104] [FE]:[feat] 네비게이션 컴포넌트 생성 #in-progress

---
+++ Frontend/cigma/src/components/pages/StartPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../organisms/NavBar';
+
+function StartPage() {
+    return (
+        <div>
+            <NavBar />
+            <div>start</div>
+        </div>
+    );
+}
+
+export default StartPage;
