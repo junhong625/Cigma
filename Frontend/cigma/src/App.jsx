1e31fc940304cf7642ed2f5a03fe9a04e3f140d6
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-04-21 16:43:46 +0900

    [FE]:[feat] Lottie 사용하여 로딩 페이지 완성

--- Frontend/cigma/src/App.jsx
+++ Frontend/cigma/src/App.jsx
@@ -2,13 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import LoadingAtom from "./components/atoms/LoadingAtom";
 import LoginPage from "./components/pages/LoginPage";
 import SignUpPage from "./components/pages/SignupPage";
 
 function App() {
   return (
     <div>
-      <SignUpPage />
+      <LoginPage />
     </div>
   );
 }
