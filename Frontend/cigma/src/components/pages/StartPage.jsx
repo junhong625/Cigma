546d5b00a9d3a9fc41ee7e57510d63d16a1bc18d
2yoonjinny <2yoonjinny@naver.com>
2023-04-21 16:10:52 +0900

    [S08P31A601-108] [FE]:[feat] startpage 헤더부분 제작 #in-progress

--- Frontend/cigma/src/components/pages/StartPage.jsx
+++ Frontend/cigma/src/components/pages/StartPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import NavBar from '../organisms/NavBar';
 import '../../styles/pages/StartPage.scss';
+import ButtonAtom from '../atoms/ButtonAtom';
 function StartPage() {
     return (
         <div className="navigation-container">
             <NavBar />
-            <div className="image-container"/>
+            <div className="image-container">
+                <div className="title-container">
+                    <label>Code + Figma = Cigma</label>
+                    <label className="sub-label">함께 코딩하는 즐거움</label>
+                    <ButtonAtom buttonName={'다운로드'} />
+                </div>
+            </div>
             <div>start</div>
             <div>온보딩첫페이지</div>
         </div>
