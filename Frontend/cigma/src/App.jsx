6de2d62349988ac1dd7d2e423693d21fd78a0b06
2yoonjinny <2yoonjinny@naver.com>
2023-04-24 14:23:12 +0900

    [S08P31A601-147] [FE]:[feat] navigate route 처리 #in-progress

--- Frontend/cigma/src/App.jsx
+++ Frontend/cigma/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
-import ProjectPage from './components/pages/ProjectPages'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import ProjectPage from './components/pages/ProjectPages';
 import ProjectList from './components/organisms/ProjectList';
 import TrashList from './components/organisms/TrashList';
+import StartPage from './components/pages/StartPage';
+import DocsPage from './components/pages/DocsPage';
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/projects" element={<ProjectPage />}>
-            <Route path="/projects" element={<ProjectList />} />
-            <Route path="/projects/trashcan" element={<TrashList />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<StartPage />}></Route>
+                    <Route path="/tutorials" element={<DocsPage />}></Route>
+                    <Route path="/projects" element={<ProjectPage />}>
+                        <Route path="/projects" element={<ProjectList />} />
+                        <Route path="/projects/trashcan" element={<TrashList />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- Frontend/cigma/src/components/organisms/NavBar.jsx
+++ Frontend/cigma/src/components/organisms/NavBar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import '../../styles/organisms/NavBar.scss';
 import NavItem from '../atoms/NavItem';
-import NavLogo from "../atoms/NavLogo";
-import NavMenu from "./NavMenu";
+import NavLogo from '../atoms/NavLogo';
+import NavMenu from './NavMenu';
+import { NavLink } from 'react-router-dom';
 
 /**
- * 
+ *
  * @returns StartPage에서 사용되는 네비게이션 바. 로고 + 중간메뉴 + 로그인버튼
  */
 function NavBar() {
     return (
         <nav>
-            <NavLogo/>
-            <NavMenu/>
-            <NavItem itemText={'test'}></NavItem>
+            <NavLogo />
+            <NavMenu />
+            <NavLink to="/login">
+                <NavItem itemText={'Login'}></NavItem>
+            </NavLink>
         </nav>
     );
 }
