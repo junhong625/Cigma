c6241d05f6ff109af221b59f3bcbf28660485ae0
안준홍 <junhong625@naver.com>
2023-04-25 13:12:56 +0900

    [S08P31A601-149] [BE]:[feat] build.grade 추가 #done
    잘못된 위치에 생성된 .gitignore 삭제

--- Backend/cigma/src/main/java/com/cigma/cigma/user/User.java
+++ Backend/cigma/src/main/java/com/cigma/cigma/user/User.java
@@ -17,27 +17,30 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @Builder
 public class User {
-    /*
-    유저 index : Primary Key, AutoIncr
-     */
+//    유저 index : Primary Key, Auto Increment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_idx")
     private Long userIdx;
 
+//    유저 이름 : max length 10, not null
     @Column(name = "user_name", length = 10, nullable = false)
     private String userName;
 
+//    유저 이메일(아이디) : max length 50, not null
     @Column(name = "user_email", length = 50, nullable = false)
     private String userEmail;
 
+//    유저 비밀번호 : not null
     @Column(name = "user_pass", nullable = false)
     private String userPass;
 
+//    생성 시간
     @CreationTimestamp
     @Column(name = "create_time")
     private Timestamp createTime;
 
+//    수정 시간
     @UpdateTimestamp
     @Column(name = "update_time")
     private Timestamp updateTime;
