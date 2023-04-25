085b6cf77225e3496e8e1b878746f86bf43e7849
안준홍 <junhong625@naver.com>
2023-04-25 11:15:41 +0900

    [S08P31A601-149] [BE]:[feat] Spring Boot 개발 환경 #done
    Spring Boot 개발 환경 설정& DB 생성 및 빌드 테스트 완료

---
+++ Backend/cigma/src/main/java/com/cigma/cigma/CigmaApplication.java
@@ -0,0 +1,13 @@
+package com.cigma.cigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CigmaApplication.class, args);
+	}
+
+}
---
+++ Backend/cigma/src/test/java/com/cigma/cigma/CigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cigma.cigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
