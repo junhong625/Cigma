1831f449d41f2dd9734f89a07153e29ca4769d7b
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-04-26 11:08:23 +0900

    [S08P31A601-161] [FE]:[feat] redux 기초 구조 설계  #done

--- Frontend/cigma-ide/src/store/codeEditorSlice.js
+++ Frontend/cigma-ide/src/store/codeEditorSlice.js
@@ -1,8 +1,62 @@
-// import { createSlice, current } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-// const codeSlice = createSlice({
-//   name: 'code',
-//   reducers: {
+const generateCodeEditor = (top, left) => ({
+  codeEditorName: "",
+  top,
+  left,
+  width: 800,
+  height: 500,
+});
 
-//   }
-// })
+const initialState = [];
+
+const codeEditorSlice = createSlice({
+  name: "codeEditor",
+  initialState,
+  reducers: {
+    // 작성중인 코드에디터 불러오기
+    loadCodeEditor: (_, { payload }) => {
+      return payload;
+    },
+    // 코드에디터 숨기기
+    hideCodeEditor: (state, { payload: codeEditorIndex }) => {
+      state.splice(codeEditorIndex, 1);
+    },
+    // 코드에디터 추가
+    showCodeEditor: (state, { payload: { top, left } }) => {
+      const newCodeEditor = {
+        ...generateCodeEditor(top, left),
+        canvasName: `canvas_${state.length}`,
+      };
+      state.push(newCodeEditor);
+    },
+    // 코드에디터 이름 바꾸기
+    changeCodeEditorName: (state, { payload: { name, codeEditorIndex } }) => {
+      state[codeEditorIndex].canvasName = name;
+    },
+    // 코드에디터 위치 수정
+    modifyCodeEditor: (state, { payload }) => {
+      const codeEditorIndex = payload.codeEditorIndex;
+      delete payload.codeEditorIndex;
+      state[codeEditorIndex] = {
+        ...state[codeEditorIndex],
+        ...payload,
+      };
+    },
+  },
+});
+
+// export const selectAllCodeEditor = (state) => state.workbench.present.canvas;
+
+// export const selectCanvasLength = (state) =>
+//   state.workbench.present.canvas.length;
+
+export const {
+  loadCodeEditor,
+  hideCodeEditor,
+  showCodeEditor,
+  changeCodeEditorName,
+  modifyCodeEditor,
+} = codeEditorSlice.actions;
+
+export default codeEditorSlice.reducer;
--- Frontend/cigma-ide/src/store/toolSlice.js
+++ Frontend/cigma-ide/src/store/toolSlice.js
@@ -1,5 +1,23 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
+const initialState = {
+  isSelectorActivated: true,
+  tools: ["seletor", "text"],
+};
 
-// }
+const toolSlice = createSlice({
+  name: "tool",
+  initialState,
+  reducers: {
+    deactivateSelector: (state) => {
+      state.isSelectorActivated = false;
+    },
+    activeSelctor: (state) => {
+      state.isSelectorActivated = true;
+    },
+  },
+});
+
+export const { activeSelctor, deactivateSelector } = toolSlice.actions;
+
+export default toolSlice.reducer;
