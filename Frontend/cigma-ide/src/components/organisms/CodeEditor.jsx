095078342f0ff21922c6fa2fc1302ee74efdb897
2yoonjinny <2yoonjinny@naver.com>
2023-05-01 17:30:07 +0900

    [S08P31A601-221] [FE]:[feat] code editor 더블클릭 처리(추가 수정 필요) #done

--- Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
+++ Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
@@ -1,13 +1,28 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./CodeEditor.module.scss";
 import { setCodeEditorIndex } from "../../store/toolSlice";
 import useDragCodeEditor from "../../hooks/useDragCodeEditor";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import EditPointer from "../atoms/EditPointer";
+import computeSelectionBox from "../../tools/computeSelectionBox";
+import { selectAllCodeEditor } from "../../store/codeEditorSlice";
 
+const directions = {
+  N: "n",
+  E: "e",
+  S: "s",
+  W: "w",
+  NE: "ne",
+  NW: "nw",
+  SE: "se",
+  SW: "sw",
+};
 const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
   const dispatch = useDispatch();
   const canvasRef = useRef();
-
+  const codeEditors = useSelector(selectAllCodeEditor);
+  // 더블클릭 -> 사이즈 조정
+  const [isDoubleClicked, setIsDoubleClicked] = useState(false);
   useDragCodeEditor(codeEditorIndex, artBoardRef, canvasRef);
 
   // 모나코 들어갈 곳
@@ -16,8 +31,22 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
       ref={canvasRef}
       className={styles["code-editor"]}
       style={{ ...codeEditor }}
+      onDoubleClick={() => {
+        console.log(`작동되는건가?`);
+        setIsDoubleClicked(true);
+      }}
       onClick={() => dispatch(setCodeEditorIndex(codeEditorIndex))}
     >
+      {isDoubleClicked
+        ? // EditPinter atoms 들어갈 자리.
+          Object.values(directions).map((direction) => (
+            <EditPointer
+              direction={direction}
+              key={direction}
+              {...computeSelectionBox(codeEditors, codeEditorIndex)}
+            />
+          ))
+        : null}
       test code editor
     </div>
   );
