ef1905700f5c11f7942a67e45e1848cb6e66eb9a
unknown <driculer@gmail.com>
2023-05-01 14:54:57 +0900

    [FE]:[feat] File Explorer [#in-progress]
    File Explorer Width Change

--- Frontend/cigma-ide/src/components/pages/WorkBenchPage.jsx
+++ Frontend/cigma-ide/src/components/pages/WorkBenchPage.jsx
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import styles from "../../styles/pages/WorkBenchPage.module.scss";
 import SideBar from "../organisms/SideBar";
 import FileTreeOrganism from "../organisms/FileTreeOrganism";
 import WorkSpacePage from "./WorkSpacePage";
 
-const WorkBenchPage = () => {
+const WorkBenchPage = ({ handleFileBar }) => {
+
+  // 왼쪽 사이드바의 너비값 설정
+  const [widthLeft, setWidthLeft] = useState(240);
+
   return (
     <div className={styles.workbench}>
       {/* <SideBar /> */}
-      <FileTreeOrganism />
-      <WorkSpacePage />
+      <FileTreeOrganism
+        handleFileBar={handleFileBar}
+        widthLeft={widthLeft}
+        setWidthLeft={setWidthLeft}
+      />
+      <WorkSpacePage handleFileBar={handleFileBar} widthLeft={widthLeft} />
       {/* <SideBar /> */}
     </div>
   );
