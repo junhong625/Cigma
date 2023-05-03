f44fd9170bd8f4db31979d7c4f115392130d3d82
2yoonjinny <2yoonjinny@naver.com>
2023-05-03 15:39:21 +0900

    [S08P31A601-220] [FE]:[feat] editor파일 dispatch 함수 추가  #in-progress

--- Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
+++ Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./CodeEditor.module.scss";
-import { setCodeEditorIndex } from "../../store/toolSlice";
+import {
+  hideEditPointer,
+  selectEditPointerVisible,
+  setCodeEditorIndex,
+  showEditPointer,
+} from "../../store/toolSlice";
 import useDragCodeEditor from "../../hooks/useDragCodeEditor";
 import { useRef, useState } from "react";
 import EditPointer from "../atoms/EditPointer";
 import computeSelectionBox from "../../tools/computeSelectionBox";
-import { selectAllCodeEditor } from "../../store/codeEditorSlice";
+import { hideCodeEditor, selectAllCodeEditor } from "../../store/codeEditorSlice";
 
 const directions = {
   N: "n",
@@ -24,18 +29,30 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
   // 더블클릭 -> 사이즈 조정
   const [isDoubleClicked, setIsDoubleClicked] = useState(false);
   useDragCodeEditor(codeEditorIndex, artBoardRef, canvasRef);
-
+  // 편집점 숨김여부
+  const isEditPointerVisible = useSelector(selectEditPointerVisible);
   // 모나코 들어갈 곳
+
+  const handleInput = (event) => {
+    setIsDoubleClicked(false);
+    dispatch(hideEditPointer);
+  };
   return (
     <>
       <div
         ref={canvasRef}
         className={styles["code-editor"]}
         style={{ ...codeEditor }}
-        onDoubleClick={() => {
-          setIsDoubleClicked(true);
+        tabIndex={0}
+        onDoubleClick={() => {}}
+        onBlur={() => {
+          handleInput();
         }}
-        onClick={() => dispatch(setCodeEditorIndex(codeEditorIndex))}
+        onClick={() => {
+          dispatch(setCodeEditorIndex(codeEditorIndex));
+          setIsDoubleClicked(true);
+          dispatch(showEditPointer());
+        }}
       >
         test code editor
         {isDoubleClicked
