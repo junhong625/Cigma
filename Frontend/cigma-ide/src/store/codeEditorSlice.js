471cb693af4cd90e32b8d6e6c0aab305b4939d5f
황진태 <jintea519@gmail.com>
2023-05-04 15:57:51 +0900

    [FE]:[feat] add redux-yjs-binding

--- Frontend/cigma-ide/src/store/codeEditorSlice.js
+++ Frontend/cigma-ide/src/store/codeEditorSlice.js
@@ -10,7 +10,7 @@ const generateCodeEditor = (top, left) => ({
 });
 
 const initialState = [generateCodeEditor(1000, 1000)];
-yLocs.set("codeEditors", initialState);
+// yLocs.set("codeEditors", initialState);
 
 const codeEditorSlice = createSlice({
   name: "codeEditor",
@@ -18,13 +18,11 @@ const codeEditorSlice = createSlice({
   reducers: {
     // 작성중인 코드에디터 불러오기
     loadCodeEditor: (_, { payload }) => {
-      // yLocs.set("codeEditors", payload);
       return payload;
     },
     // 코드에디터 숨기기
     hideCodeEditor: (state, { payload: codeEditorIndex }) => {
       state.splice(codeEditorIndex, 1);
-      // yLocs.set("codeEditors", state);
     },
     // 코드에디터 추가
     showCodeEditor: (state, { payload: { top, left } }) => {
@@ -34,12 +32,10 @@ const codeEditorSlice = createSlice({
         canvasName: `canvas_${state.length}`,
       };
       state.push(newCodeEditor);
-      // yLocs.set("codeEditors", state);
     },
     // 코드에디터 이름 바꾸기
     changeCodeEditorName: (state, { payload: { name, codeEditorIndex } }) => {
       state[codeEditorIndex].canvasName = name;
-      // yLocs.set("codeEditors", state);
     },
     // 코드에디터 위치 수정
     modifyCodeEditor: (state, { payload }) => {
@@ -49,7 +45,6 @@ const codeEditorSlice = createSlice({
         ...state[codeEditorIndex],
         ...payload,
       };
-      // yLocs.set("codeEditors", state);
     },
     // 코드에디터 크기 수정
     resizeNorth: (state, { payload }) => {
@@ -116,7 +111,6 @@ const codeEditorSlice = createSlice({
 });
 
 export const selectAllCodeEditor = (state) => state.workbench.codeEditor;
-// export const selectAllCodeEditor = () => yLocs.get("codeEditors");
 
 // export const selectCanvasLength = (state) =>
 //   state.workbench.present.canvas.length;
--- Frontend/cigma-ide/src/store/configureStore.js
+++ Frontend/cigma-ide/src/store/configureStore.js
@@ -3,6 +3,9 @@ import defaultTextSlice from "./defaultTextSlice";
 import codeEditorSlice from "./codeEditorSlice";
 import toolSlice from "./toolSlice";
 import textSlice from "./textSlice";
+
+import { bind, enhanceReducer } from "redux-yjs-bindings";
+import { ydoc } from "./initYDoc";
 // import undoable from "redux-undo";
 
 // const MAXIMUN_UNDO_COUNT = 100;
@@ -21,10 +24,13 @@ const workbench = combineReducers({
 
 const store = configureStore({
   reducer: {
-    workbench: workbench,
+    workbench: enhanceReducer(workbench),
     tool: toolSlice,
   },
 });
+
+bind(ydoc, store, "workbench");
+// bind(ydoc, store, "tool");
 
 export default store;
 
