a6e5fd6ade8d3682bc94916dcd55f04fc30f0724
안준홍 <junhong625@naver.com>
2023-05-08 09:54:23 +0900

    [BE]:[feat] Spring boot cors 처리
    WebMvcConfigurer addCorsMappings 설정

--- Backend/cigma/src/main/java/com/cigma/cigma/CigmaApplication.java
+++ Backend/cigma/src/main/java/com/cigma/cigma/CigmaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableConfigurationProperties({JwtProperties.class, RedisProperties.class})
@@ -14,4 +17,13 @@ public class CigmaApplication {
 		SpringApplication.run(CigmaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("k8a601.p.ssafy.io:9090");
+			}
+		};
+	}
 }
