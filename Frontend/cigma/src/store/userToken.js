3093bf98c673bdf289f10e2d64905dc5f9ec7c41
2yoonjinny <2yoonjinny@naver.com>
2023-05-12 17:21:30 +0900

    [S08P31A601-244][FE]:[chore] token 삭제 redux store처리 #in-progress

--- Frontend/cigma/src/store/userToken.js
+++ Frontend/cigma/src/store/userToken.js
@@ -1,5 +1,9 @@
+// 로그인할때 새로 받는 토큰
 const MODIFY_USERTOKEN = "userToken/MODIFY_USERTOKEN";
+
 const INIT_USERTOKEN = "userToken/INIT_USERTOKEN";
+// 로그아웃 시 토큰 삭제
+const DELETE_USERTOKEN = "userToken/DELETE_USERTOKEN";
 
 // 액션 생성함수를 만든다.
 export const modifyUserToken = (data) => ({
@@ -7,7 +11,7 @@ export const modifyUserToken = (data) => ({
   type: MODIFY_USERTOKEN,
 });
 export const initUserToken = () => ({ type: INIT_USERTOKEN });
-
+export const deleteUserToken = () => ({ type: DELETE_USERTOKEN });
 // 초기값 제작
 const initialState = "";
 
@@ -17,6 +21,8 @@ export default function userToken(state = initialState, action) {
     case MODIFY_USERTOKEN:
       return action.data;
     case INIT_USERTOKEN:
+      return initialState;
+    case DELETE_USERTOKEN:
       return initialState;
     default:
       return state;
