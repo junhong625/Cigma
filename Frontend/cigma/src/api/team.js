85039285c626085f33a603f58e31abceb5ee4157
2yoonjinny <2yoonjinny@naver.com>
2023-05-12 15:48:13 +0900

    [S08P31A601-252][FE]:[Feat] api 추가 #in-progress

--- Frontend/cigma/src/api/team.js
+++ Frontend/cigma/src/api/team.js
@@ -5,18 +5,53 @@ export const callTeams = async (token) => {
   const api = createApi({ token });
   try {
     const response = await api.get(`/user/team`);
+    console.log(JSON.stringify(response));
     return {
       status: response.status,
-      teamList: response.body.data,
+      teamList: response.data,
     };
   } catch (error) {
     console.error("browse error", error);
     return {
-      status: error.response.status,
+      status: error,
     };
   }
 };
 
 //팀 생성
+export const createTeams = async (token, teamName) => {
+  const api = createApi({ token });
+  try {
+    const response = await api.post(`/team`, {
+      teamName: teamName,
+    });
+    return {
+      status: response.status,
+    };
+  } catch (error) {
+    console.error("team creation error", error);
+    return {
+      status: error,
+    };
+  }
+};
 
-//팀 삭제
+/**
+ * 팀 삭제 API
+ * @params teamIdx
+ */
+
+export const deleteTeam = async (token, teamIdx) => {
+  const api = createApi({ token });
+  try {
+    const response = await api.delete(`/team/${teamIdx}`);
+    return {
+      status: response.status,
+    };
+  } catch (error) {
+    console.error("team delete error", error);
+    return {
+      status: error.response.status,
+    };
+  }
+};
