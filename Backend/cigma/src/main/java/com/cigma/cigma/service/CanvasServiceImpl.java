63b493dec6f82468954926311838152389618cf2
junhong625 <83000975+junhong625@users.noreply.github.com>
2023-05-15 04:15:13 +0900

    [BE]:[chore] docker network rollback

--- Backend/cigma/src/main/java/com/cigma/cigma/service/CanvasServiceImpl.java
+++ Backend/cigma/src/main/java/com/cigma/cigma/service/CanvasServiceImpl.java
@@ -210,7 +210,7 @@ public class CanvasServiceImpl implements CanvasService{
     }
 
     public void connect() throws Exception {
-        ApiClient client = Config.defaultClient();
+        ApiClient client = Config.defaultClient().setBasePath("http://k8a601.p.ssafy.io:6443");
         Configuration.setDefaultApiClient(client);
         api = new CoreV1Api();
         log.info("connect k3s");
