1f2543d834965f88e439d4f33541332ed6a77183
junhong625 <83000975+junhong625@users.noreply.github.com>
2023-05-15 04:01:02 +0900

    [BE]:[chore] docker network change

--- Backend/cigma/src/main/java/com/cigma/cigma/service/CanvasServiceImpl.java
+++ Backend/cigma/src/main/java/com/cigma/cigma/service/CanvasServiceImpl.java
@@ -210,7 +210,7 @@ public class CanvasServiceImpl implements CanvasService{
     }
 
     public void connect() throws Exception {
-        ApiClient client = Config.defaultClient().setBasePath("http://host.docker.internal:6443");
+        ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
         api = new CoreV1Api();
         log.info("connect k3s");
