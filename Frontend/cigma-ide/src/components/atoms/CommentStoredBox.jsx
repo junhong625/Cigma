75fbf07ede89a5eb57fd7d7b8157d5d670e46854
2yoonjinny <2yoonjinny@naver.com>
2023-05-15 17:56:04 +0900

    [FE]:[feat] comment create css수정

--- Frontend/cigma-ide/src/components/atoms/CommentStoredBox.jsx
+++ Frontend/cigma-ide/src/components/atoms/CommentStoredBox.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styles from "../../styles/atoms/CommentStoredBox.module.scss";
+import { MdOutlineAccessTimeFilled } from "react-icons/md";
 /**
  * 댓글 리스트들 보여주는 컴포넌트
  * @returns
  */
-const CommentStoredBox = ({ comment }) => {
+const CommentStoredBox = ({ comment, userId, timestamp }) => {
   return (
     <div className={styles["comment-container"]}>
-      <div>{comment}</div>
-      <div>{comment}</div>
+      <div className={styles["user-div"]}>{userId}</div>
+      <div className={styles["text-div"]}>{comment}</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row-reverse",
+          color: "gray",
+        }}
+      >
+        <div className={styles["time-div"]}>{timestamp}</div>
+        <MdOutlineAccessTimeFilled />
+      </div>
     </div>
   );
 };
