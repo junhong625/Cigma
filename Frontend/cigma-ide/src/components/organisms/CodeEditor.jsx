3fa727b4becc72406d1da91f8e35cf8bbea28ac9
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-05-16 10:34:56 +0900

    [FE]:[feat] 이용중일 때 이동 및 편집 처리

--- Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
+++ Frontend/cigma-ide/src/components/organisms/CodeEditor.jsx
@@ -59,9 +59,7 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
     shownColor,
     editorPerson,
   } = codeEditor;
-  // const [myWorking, setMyWorking] = useState(null);
-  // 더블클릭 -> 에디터편집
-  const [isDoubleClicked, setIsDoubleClicked] = useState(false);
+
   // 에디터 상단 바
   // const [isHidden, setIsHidden] = useState(false);
   const isDragScrolling = useSelector(selectIsDragScrolling);
@@ -75,20 +73,16 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
   // 단축키 추가
   // useGlobalKeyboardShortCut(isClicked);
 
-  // 더블클릭 이벤트 처리
-  const handleDoubleClick = () => {
-    setIsDoubleClicked(true);
-  };
-
   // div 포커스 해제되었을때 처리되는 핸들러
   const myColor = awareness.getLocalState().color;
   const myName = awareness.getLocalState().name;
 
   const handleBlurred = (event) => {
-    console.log("blurred");
     setIsClicked(false);
-    setIsDoubleClicked(false);
     dispatch(hideEditPointer);
+    dispatch(
+      changeShownColor({ color: null, codeEditorIndex: codeEditorIndex })
+    );
     if (editorPerson !== myName) return;
     handleFinishIsShown();
     dispatch(detachFile);
@@ -121,12 +115,17 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
     );
     dispatch(setInputFieldFocused());
   };
+  const changeColor = () => {
+    dispatch(
+      changeShownColor({ color: myColor, codeEditorIndex: codeEditorIndex })
+    );
+  };
   const handleFinishIsShown = () => {
     dispatch(setFinishIsShown({ codeEditorIndex: codeEditorIndex }));
     dispatch(setEditorPerson({ name: null, codeEditorIndex: codeEditorIndex }));
-    dispatch(
-      changeShownColor({ color: null, codeEditorIndex: codeEditorIndex })
-    );
+    // dispatch(
+    //   changeShownColor({ color: null, codeEditorIndex: codeEditorIndex })
+    // );
     dispatch(setInputFieldBlurred());
   };
 
@@ -136,20 +135,22 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
   const isHiddenStyle = {
     ...codeEditor,
     height: isHidden ? "30px" : height,
-    border: isShown ? `2px solid ${shownColor}` : "none",
+    border: shownColor ? `2px solid ${shownColor}` : "none",
   };
 
   return (
     <div
       className={styles["code-editor"]}
-      onClick={() => {
+      onClick={(event) => {
+        changeColor();
+        event.preventDefault();
+      }}
+      onDoubleClick={() => {
         handleStartIsShown();
         if (isShown) {
-          console.log(isShown);
           if (editorPerson === null || myName === editorPerson) {
             dispatch(setCodeEditorIndex(codeEditorIndex));
             setIsClicked(true);
-            //setIsDoubleClicked(true);
             dispatch(showEditPointer());
           }
         }
@@ -160,7 +161,6 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
           })
         );
       }}
-      onDoubleClick={handleDoubleClick}
       onBlur={() => {
         // TODO: 하이라이트 해제되었을 떄 수정 필요 (한나/윤진)
         // handleFinishIsShown();
@@ -239,7 +239,6 @@ const CodeEditor = ({ codeEditorIndex, artBoardRef, ...codeEditor }) => {
             className={styles["monaco-editor"]}
             file={codeEditors[codeEditorIndex].canvasName}
             fileType={codeEditors[codeEditorIndex].fileType}
-            readOnly={!isDoubleClicked}
             style={{ height: height - 30 }}
           />
           {/* comment 화면 처리 */}
