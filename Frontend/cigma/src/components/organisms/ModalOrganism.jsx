0f702c5b49d30f8ee0d770e0f02b1a85481e7d0e
2yoonjinny <2yoonjinny@naver.com>
2023-05-17 17:57:13 +0900

    [FE]:[feat] project empty 임의 설정중

--- Frontend/cigma/src/components/organisms/ModalOrganism.jsx
+++ Frontend/cigma/src/components/organisms/ModalOrganism.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
-function ModalOrganism() {
+function ModalOrganism({ type, action, setModalOpen, apiFunc }) {
+  // project 생성
+  /**
+   * 상위 컴포넌트에서 세팅해야할 것
+   1. * 액션 클릭했을때 Modal 컴포넌트 보여주기
+  const [modalOpen, setModalOpen] = useState(false);
+  <button onClick={() => setModalOpen(true)}>클릭해야할것</button>
+
+  2. props 넘겨주기
+  - type : 종류 (일기장, 일기, 댓글, 대나무, 대나무잎)
+  - action : 삭제, 신고
+  - apiFunc : 예 선택시 실행할 함수
+  {modalOpen && (
+    <Modal type={'댓글신고'} setModalOpen={setModalOpen} apiFunc={api 요청 보낼 함수} />
+    )}
+    */
   return <div>ModalOrganism</div>;
 }
 
