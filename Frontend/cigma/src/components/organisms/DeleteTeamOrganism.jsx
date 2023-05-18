f21f678aa00a1d84537c1f6da9396b70c0da5809
2yoonjinny <2yoonjinny@naver.com>
2023-05-18 14:36:03 +0900

    [FE]:[feat] 팀 삭제 모달 생성

--- Frontend/cigma/src/components/organisms/DeleteTeamOrganism.jsx
+++ Frontend/cigma/src/components/organisms/DeleteTeamOrganism.jsx
@@ -1,11 +1,38 @@
-import React from "react";
-import styles from "../../styles/organisms/CreateProjectOrganism.module.scss";
+import React, { useState } from "react";
+import styles from "../../styles/organisms/DeleteTeamOrganism.module.scss";
+import { deleteTeam } from "../../api/team";
+import ButtonAtom from "../atoms/ButtonAtom";
+import { useSelector } from "react-redux";
 
-function DeleteTeamOrganism({ closeModal }) {
+//const deleteTeam = async (token, teamIdx)
+function DeleteTeamOrganism({ closeModal, teamIdx }) {
+  // const [inputValue, setInputValue] = useState("");
+  // usertoken
+  const userToken = useSelector((store) => store.userToken);
+  // api 호출
+  const makeTeamRemoved = async () => {
+    const { status } = await deleteTeam(userToken, teamIdx);
+    if (status === 200) {
+      // 삭제
+    }
+  };
   return (
-    <div>
-      <div>팀을 삭제하시겠습니까?</div>
-      <div>포함된 프로젝트들이 삭제되며, 복구되지 않습니다.</div>
+    <div className={styles.app}>
+      <div className={styles.title}>팀 삭제 이후 복구가 불가합니다.</div>
+      <div className={styles.buttonWrapper}>
+        <ButtonAtom
+          onClick={() => {
+            makeTeamRemoved();
+          }}
+          buttonName={"네"}
+          style={{ color: "#fefefe", width: "30%" }}
+        />
+        <ButtonAtom
+          onClick={closeModal}
+          buttonName={"아니오"}
+          style={{ color: "#fefefe", width: "30%" }}
+        />
+      </div>
     </div>
   );
 }
