fec375756cea31ccbaf58f4bbfe5f9b608164ab0
2yoonjinny <2yoonjinny@naver.com>
2023-05-18 11:29:32 +0900

    [FE]:[feat] 프로젝트 생성 모달 제작

--- Frontend/cigma/src/components/organisms/CreateProjectOrganism.jsx
+++ Frontend/cigma/src/components/organisms/CreateProjectOrganism.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import styles from "../../styles/organisms/CreateProjectOrganism.module.scss";
+import { useNavigate } from "react-router-dom";
+import CreateProjectForm from "../atoms/CreateProjectForm";
+import { addProject } from "../../api/project";
+// api:: createProject = async (token, projectName, teamIdx)
+function CreateProjectOrganism({ closeModal, teamIdx }) {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+  // usertoken
+  const userToken = useSelector((store) => store.userToken);
+  // api 호출
+  const createProject = async () => {
+    const { status, projectInfo } = await addProject(userToken, inputValue, teamIdx);
+    // 프로젝트 생성 성공
+    if (status === 200) {
+      alert("프로젝트를 생성했습니다. ");
+    }
+  };
 
-function CreateProjectOrganism({ closeModal }) {
-  return <div>프로젝트생성</div>;
+  return (
+    <div className={styles.app}>
+      <div className={styles.title}>프로젝트 생성</div>
+      <CreateProjectForm
+        text={inputValue}
+        setText={setInputValue}
+        apiFunc={createProject}
+        placeholder={"프로젝트의 이름을 작성해주세요."}
+      />
+    </div>
+  );
 }
 
 export default CreateProjectOrganism;
