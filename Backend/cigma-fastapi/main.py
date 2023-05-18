a5e104a0999ae6cfc2fd446ce6db805eb32de7b8
황진태 <jintea519@gmail.com>
2023-05-18 11:40:23 +0900

    [BE]:[fix] start uvicorn

--- Backend/cigma-fastapi/main.py
+++ Backend/cigma-fastapi/main.py
@@ -1,6 +1,6 @@
 import os
 import docker
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 
 client = docker.from_env()
@@ -27,10 +27,11 @@ def info():
 
 
 @app.post("/ide/create")
-def create_container(createModel: CreateModel):
+def create_container(createModel: CreateModel, response: Response):
     create_dict = createModel.dict()
     print(create_dict)
     if create_dict == None or create_dict["teamName"] == "" or create_dict["projectName"] == "":
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {"status": 400}
     default_name = f'{create_dict["teamName"]}_{create_dict["projectName"]}'
     default_path = f'/home/ubuntu/canvas/{default_name}/workspace'
@@ -47,20 +48,24 @@ def create_container(createModel: CreateModel):
         print("container_start")
     except Exception as e:
         print(e)
+        response.status_code = status.HTTP_409_CONFLICT
         return {"msg": e, "status": 409}
+    response.status_code = status.HTTP_200_OK
     return {"containerId": container.id, "status": 200}
 
 
 @app.post("/ide/delete")
-def delete_container(deleteModel: DeleteModel):
+def delete_container(deleteModel: DeleteModel, response: Response):
     delete_dict = deleteModel.dict()
     if delete_dict == None or delete_dict["containerId"] == "":
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {"status": 400}
 
     resConainerId = delete_dict["containerId"]
     try:
         container = client.containers.get(resConainerId)
     except Exception as e:
+        response.status_code = status.HTTP_404_NOT_FOUND
         return {"msg": e, "status": 404}
     container.stop()
     container.remove()
