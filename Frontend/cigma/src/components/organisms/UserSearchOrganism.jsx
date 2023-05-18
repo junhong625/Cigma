f4ef6532aafcbbc998645c88738976bd5e11a259
2yoonjinny <2yoonjinny@naver.com>
2023-05-18 09:17:35 +0900

    [FE]:[feat] 팀원초대처리

--- Frontend/cigma/src/components/organisms/UserSearchOrganism.jsx
+++ Frontend/cigma/src/components/organisms/UserSearchOrganism.jsx
@@ -1,24 +1,42 @@
-import {useState } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "../../styles/organisms/UserSearchOrganism.module.scss";
 import CreateProjectForm from "../atoms/CreateProjectForm";
+import { addMember } from "../../api/team";
 
 /**
- * 
+ *
  * @param closeModal 모달 닫기 설정
  */
 function UserSearch({ closeModal, teamIdx }) {
   const [inputValue, setInputValue] = useState("");
-  const inviteMember = () => { 
-    // 팀원 초대하기
-  }
+  const navigate = useNavigate();
+  //유저토큰
+  const userToken = useSelector((store) => store.userToken);
+  const inviteMember = async () => {
+    const { status, team } = await addMember(userToken, teamIdx, inputValue);
+    // 팀원 초대 성공
+    if (status === 200) {
+      alert("초대에 성공했습니다");
+      console.log(`team:::${team}`);
+      navigate("/projects");
+    }
+    if (status === 401) {
+      alert("팀원 초대는 팀장만 가능합니다");
+    }
+  };
   return (
     <div className={styles.app}>
-      <div className={styles.title}>사용자 검색
-        
-      </div>
-      <CreateProjectForm text={inputValue} setText={setInputValue} apiFunc={ inviteMember} placeholder={"초대할 사용자 아이디 입력"} />
+      <div className={styles.title}>사용자 검색</div>
+      <CreateProjectForm
+        text={inputValue}
+        setText={setInputValue}
+        apiFunc={inviteMember}
+        placeholder={"초대할 사용자 아이디 입력"}
+      />
     </div>
-  )
+  );
 }
 
 export default UserSearch;
--- Frontend/cigma/src/components/pages/ProjectPage.jsx
+++ Frontend/cigma/src/components/pages/ProjectPage.jsx
@@ -35,21 +35,20 @@ function ProjectPage() {
 
   // 유저가 갖고 있는 팀 리스트 호출 (현재는 임시데이터)
   const [teamList, setTeamList] = useState([
-    {
-      teamIdx: 1,
-      teamLeader: "재히",
-      teamName: "재희없는재희팀",
-      teamImageUrl:
-        "https://camo.githubusercontent.com/809619bf25026aa02652a2bc41a03e58856236d3dfe73c2794a18b3fe8e29d3b/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4a6176615363726970742d4637444631452e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d4a617661536372697074266c6f676f436f6c6f723d626c61636b",
-    },
-    {
-      teamIdx: 1,
-      teamLeader: "찬빈",
-      teamName: "찬빈없는찬빈팀",
-      teamImageUrl:
-        "https://camo.githubusercontent.com/1026f577af0676ea62dd6389c6063a4888f222d8775a997dcf0236dd702b6408/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146422e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d5265616374266c6f676f436f6c6f723d626c61636b",
-    },
-
+    // {
+    //   teamIdx: 1,
+    //   teamLeader: "재히",
+    //   teamName: "재희없는재희팀",
+    //   teamImageUrl:
+    //     "https://camo.githubusercontent.com/809619bf25026aa02652a2bc41a03e58856236d3dfe73c2794a18b3fe8e29d3b/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4a6176615363726970742d4637444631452e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d4a617661536372697074266c6f676f436f6c6f723d626c61636b",
+    // },
+    // {
+    //   teamIdx: 1,
+    //   teamLeader: "찬빈",
+    //   teamName: "찬빈없는찬빈팀",
+    //   teamImageUrl:
+    //     "https://camo.githubusercontent.com/1026f577af0676ea62dd6389c6063a4888f222d8775a997dcf0236dd702b6408/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146422e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d5265616374266c6f676f436f6c6f723d626c61636b",
+    // },
   ]);
 
   const updateTeamList = (newTeamList) => {
